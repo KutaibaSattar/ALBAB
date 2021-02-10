58d115cc46b251cb697fe29a3416e81e9e9a723b
Kutaiba Sattar <kutaiba.sattar@gmail.com>
2021-02-10 15:45:16 +0400

    InstallAngular

--- Entities/AppUser/AppUser.cs
+++ Entities/AppUser/AppUser.cs
@@ -10,10 +10,11 @@ namespace ALBaB.Entities
       [Required(ErrorMessage = "You must provide a phone number")]
       [Phone]
       public override string PhoneNumber { get; set; }
-   
       public DateTime Created { get; set; }   = DateTime.Now;
-
        public DateTime LastActive { get; set; } =  DateTime.Now; 
+       public string Introduction { get; set; }
+       public string LookingFor { get; set; }
+       public string Interests { get; set; }
 
        public Address Address {get;set;}    
       
